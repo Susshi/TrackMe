860d95101d686dc1ccc719217797daea763a7fda
Bennet Sartori <y0035230@izcip07.ibr.cs.tu-bs.de>
2012-05-15 16:41:48 +0200

    Custom Button erneut klickbar

--- src/de/androidlab/trackme/activities/MapActivity.java
+++ src/de/androidlab/trackme/activities/MapActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RadioButton;
@@ -28,29 +29,48 @@ public class MapActivity extends Activity {
         // Home Button Events
         Button homeBtn = (Button)findViewById(R.id.mapview_btn_home);             
         homeBtn.setOnClickListener(new HomeButtonListener(this));
+        
         // Back Button Events
         Button backBtn = (Button)findViewById(R.id.mapview_btn_back);             
         backBtn.setOnClickListener(new BackButtonListener(this));
+        
         // Radiobutton Custom Events
         RadioButton customBtn = (RadioButton)findViewById(R.id.mapview_radio_custom);
         customBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                customButtonChanged(buttonView, isChecked);
+                if (!isChecked) lastActive = buttonView;
             }
         });
+        customBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				startActivityForResult(new Intent(MapActivity.this, RouteListActivity.class), SHOWROUTESREQUEST);
+			}
+        });
+        
         // Radiobutton Friends Events
         RadioButton friendsBtn = (RadioButton)findViewById(R.id.mapview_radio_friends);
         friendsBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                friendsButtonChanged(buttonView, isChecked);
+            	if (!isChecked) lastActive = buttonView;
             }
         });
+        friendsBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO
+			}
+        });
+        
         // Radiobutton All Events
         RadioButton allBtn = (RadioButton)findViewById(R.id.mapview_radio_all);
         allBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                allButtonChanged(buttonView, isChecked);
+            	if (!isChecked) lastActive = buttonView;
             }
+        });
+        allBtn.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				// TODO
+			}
         });
     }
     
@@ -58,33 +78,11 @@ public class MapActivity extends Activity {
         switch(requestCode) {
         case SHOWROUTESREQUEST: if (resultCode == Activity.RESULT_OK) {
                                     processShowRoutesResult(data);
+                                    lastActive = (RadioButton)findViewById(R.id.mapview_radio_custom);
                                 } else {
                                     lastActive.setChecked(true);
                                 }
                                 break;
-        }
-    }
-    
-    private void allButtonChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
-            // TODO
-        } else {
-            lastActive = buttonView;
-        } 
-    }
-    
-    private void friendsButtonChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
-        } else {
-            lastActive = buttonView;
-        }  
-    }
-    
-    private void customButtonChanged(CompoundButton buttonView, boolean isChecked) {
-        if (isChecked) {
-            startActivityForResult(new Intent(MapActivity.this, RouteListActivity.class), SHOWROUTESREQUEST);
-        } else {
-            lastActive = buttonView;
         }
     }
 
