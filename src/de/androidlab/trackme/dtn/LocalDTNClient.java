b9693b46806b227ddb523b50115f73e13f08af67
Christian Brümmer <christian.bruemmer@gmx.de>
2012-07-04 00:11:35 +0200

    - close crash fixed

--- src/de/androidlab/trackme/dtn/LocalDTNClient.java
+++ src/de/androidlab/trackme/dtn/LocalDTNClient.java
@@ -1,7 +1,15 @@
 package de.androidlab.trackme.dtn;
 
+import java.nio.ByteBuffer;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.StringTokenizer;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -31,16 +39,61 @@ import de.tubs.ibr.dtn.api.SingletonEndpoint;
 
 public class LocalDTNClient {
 	
+	// valid package types
+	enum PacketType
+	{
+		PRESENCE,
+		DATA
+	}
+	
+	// global tag for LogCat
 	private final static String LOGTAG = "LocalDTNClient";
+	
+	// size of type identification in header
+	private final static int HEADER_TYPE_SIZE = 4;
+	// size of the payload length information
+	private final static int HEADER_LENGTH_SIZE = 4;
+	// entire header size
+	private final static int HEADER_SIZE = HEADER_TYPE_SIZE + HEADER_LENGTH_SIZE;
+	// 15 minutes default retransmission time
+	private final static int DEFAULT_RETRANSMISSION_TIME = 1000 * 60 * 15;
+	// default presence notification delay - 5 seconds
+	private final static int DEFAULT_PRESENCE_NOTIFICATION_DELAY = 1000 * 5;
+	// default presence ttl - 5 seconds
+	private final static int DEFAULT_PRESENCE_TTL = 5;
+	// default data ttl - 120 seconds
+	private final static int DEFAULT_DATA_TTL = 20;
+	
+	private int mRetransmissionTime, mPresenceNotificationDelay, mPresenceTTL, mDataTTL;
+	// global package name
 	private String mPackageName = getClass().getPackage().getName();
+	
+	// DTN-GroupEndpoint name for presence notification
 	public static final GroupEndpoint PRESENCE_GROUP_ID = new GroupEndpoint("dtn://trackme.dtn/presence");
+	
+	// application context
 	private Context mContext = null;
 	
 	// executor to process local job queue
 	private ExecutorService mExecutor = null;
 	
-	// DTN client to talk with the DTN service
-	private LDTNClient mClient = null;	
+	// DTN-client to talk with the DTN service
+	private LDTNClient mClient = null;
+	
+	// Timer for presence notification
+	PresenceTimerTask mTask = new PresenceTimerTask();
+	Timer mTimer = new Timer();
+	
+	// presence organization
+	Map<String, Long> mPresenceMap = new HashMap<String, Long>();
+	
+	// first time init flag
+	boolean mInit;
+	
+	void localDTNClient()
+	{
+		mInit = false;
+	}
 	
 	protected class LDTNClient extends DTNClient {
 		
@@ -99,7 +152,32 @@ public class LocalDTNClient {
 		}
 	};
 	
+	public void setPresenceTTL(int time) {	// in seconds
+		mPresenceTTL = time;
+	}
+	
+	
+	public void setDataTTL(int time) { // in seconds
+		mDataTTL = time;
+	}
+		
+	public void setRetransmissionTime(int time)	// in minutes
+	{
+		mRetransmissionTime = 1000 * 60 * time;
+	}
+	
+	public void setPresenceNotificationDelay(int time) // in milliseconds
+	{
+		mPresenceNotificationDelay = time;
+	}
+	
 	public void init(Context context, String packageName) {
+		Log.i(LOGTAG, "INIT DTN");
+		if(mInit) return;
+		mRetransmissionTime = DEFAULT_RETRANSMISSION_TIME;
+		mPresenceNotificationDelay = DEFAULT_PRESENCE_NOTIFICATION_DELAY;
+		mPresenceTTL = DEFAULT_PRESENCE_TTL;
+		mDataTTL = DEFAULT_DATA_TTL;
 		mPackageName = packageName;
         mContext = context;
         // create a new executor
@@ -127,10 +205,13 @@ public class LocalDTNClient {
 		} catch (ServiceNotAvailableException e) {
 			showInstallServiceDialog(context);
 		}
-        
+		
+		// start presence notification timer
+		mTimer.scheduleAtFixedRate(mTask, mPresenceNotificationDelay, mPresenceNotificationDelay);
+        mInit = true;
 		Log.d(LOGTAG, "DTN-Client created");
     }
-    
+
 	private void showInstallServiceDialog(final Context context) {
 		DialogInterface.OnClickListener dialogClickListener = new DialogInterface.OnClickListener() {
 		    @Override
@@ -158,21 +239,25 @@ public class LocalDTNClient {
 	
 	public void close(Context context)
 	{
-		// unregister intent receiver
-		context.unregisterReceiver(mReceiver);
+		mTimer.cancel();
+		mTimer.purge();
 		
+		try { // unregister intent receiver
+			context.unregisterReceiver(mReceiver);
+		} catch(IllegalArgumentException e) {
+			
+		}
+	
 		// unregister at the daemon
 		mClient.unregister();
 		
 		try {
 			// stop executor
 			mExecutor.shutdown();
-			
 			// ... and wait until all jobs are done
 			if (!mExecutor.awaitTermination(10, TimeUnit.SECONDS)) {
 				mExecutor.shutdownNow();
 			}
-			
 			// destroy DTN client
 			mClient.terminate();
 		} catch (InterruptedException e) {
@@ -182,6 +267,7 @@ public class LocalDTNClient {
 		// clear all variables
 		mExecutor = null;
 		mClient = null;
+		mInit = false;
 	}
 
    private DataHandler mHandler = new DataHandler() {
@@ -224,39 +310,101 @@ public class LocalDTNClient {
 		}
 
 		@Override
-
 		public void payload(byte[] data) {
-			String msg = new String(data);
-			Log.i(LOGTAG, "New incoming message from" + current.source + ": " + msg);
+			final String endpoint = current.source;
+			final byte[] payload = data;
+			Log.d(LOGTAG, "Incoming packet. Size: " + payload.length);
 			mExecutor.execute(new Runnable() {
 		        public void run() {
-					try {
-						sendMessage(new String("This is the answer!").getBytes(), current.source);
-					} catch (Exception e) {
-						Log.e(LOGTAG, "Could not send message to endpoint!");
-					}
+		        	if(payload.length >= HEADER_SIZE)
+		        	{		        		
+		        		ByteBuffer bb = ByteBuffer.wrap(payload);
+		        		bb.position(0);
+		        		int type = bb.getInt();
+		        		int size = bb.getInt();
+		        		if(bb.remaining() >= size)
+		        		{
+		        			byte[] pay = new byte[size];
+		        			bb.get(pay);
+		        			processIncomingMessage(PacketType.values()[type], pay, endpoint);
+		        		}
+		        		
+		        	}
 		        }});
-		        
-			/*
-			String answer = "This is a answer!";
-			try {
-				//sendMessage(answer.getBytes(), current.source);
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			*/
-			
-			/* TODO
-			if (current.destination.equalsIgnoreCase(PRESENCE_GROUP_EID.toString()))
+		}
+		
+		private void processIncomingMessage(PacketType type, byte[] payload, String srcEndpoint)
+		{
+			Log.d(LOGTAG, "PacketType: " + type.ordinal() + " Payload size: " + payload.length);
+			switch(type)
 			{
-				eventNewPresence(current.source, current.timestamp, msg);
+			case PRESENCE:
+				Log.d(LOGTAG, "processIncomingMessage: PRESENCE");
+				long currentTime = System.currentTimeMillis();
+				if(!mPresenceMap.containsKey(srcEndpoint))
+				{
+					// fill in time if not existing before
+					mPresenceMap.put(srcEndpoint, currentTime);
+					Log.d(LOGTAG, "processIncomingMessage: new endpoint");
+				}
+				Log.d(LOGTAG, "processIncomingMessage: time elapsed: " + (currentTime - mPresenceMap.get(srcEndpoint)));
+				if((currentTime - mPresenceMap.get(srcEndpoint)) > mRetransmissionTime)
+				{
+					Log.d(LOGTAG, "processIncomingMessage: time elapsed !");
+					// update time
+					mPresenceMap.put(srcEndpoint, currentTime);
+					
+					// send data to endpoint!
+					Vector<String> strings = new Vector<String>();	// getting string vector
+					strings.add("Test1;");
+					strings.add("Test2;");
+					strings.add("Test3;");
+					String result = new String();
+					for(int i = 0; i < strings.size(); i++)
+					{
+						result = result + strings.get(i);
+						if((i + 1) < strings.size())
+							result = result + "$";
+					}
+					
+					// create header and attach payload
+					byte[] p = new byte[HEADER_SIZE + result.length()];
+					
+					ByteBuffer bb = ByteBuffer.wrap(p);
+					bb.position(0);
+					bb.putInt(PacketType.DATA.ordinal());
+					bb.putInt(result.length());
+					bb.put(result.getBytes());
+					bb.position(0);
+					bb.get(p);
+					
+					Log.d(LOGTAG, result);
+					try {
+						sendMessage(p, srcEndpoint, mPresenceTTL);
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					
+				}
+				break;
+				
+			case DATA:
+				Log.d(LOGTAG, "processIncomingMessage: DATA");
+				String data = new String(payload);
+				// split string into parts separated by '$'
+				String[] parts = data.split( "\\$" );
+
+		        //create list for this object array.
+		        List<String> list = Arrays.asList(parts);
+
+		        // create vector for given list
+		        Vector<String> vector = new Vector<String>(list);
+		        for(int i = 0; i < vector.size(); i++)
+		        	Log.d(LOGTAG, vector.get(i));
+		        // parse vector to db
+				break;
 			}
-			else
-			{
-				eventNewMessage(current.source, current.timestamp, msg);
-			}
-			*/
 		}
 
 		@Override
@@ -271,72 +419,40 @@ public class LocalDTNClient {
 		@Override
 		public void finished(int startId) {
 		}
-    
-		private void eventNewPresence(String source, Date created, String payload)
-		{
-			Log.i(LOGTAG, "Presence received from " + source);
-			
-			// buddy info
-			String nickname = null;
-			String presence = null;
-			String status = null;
-			
-			StringTokenizer tokenizer = new StringTokenizer(payload, "\n");
-			while (tokenizer.hasMoreTokens())
-			{
-				String data = tokenizer.nextToken();
-				
-				// search for the delimiter
-				int delimiter = data.indexOf(':');
-				
-				// if the is no delimiter, ignore the line
-				if (delimiter == -1) return;
-				
-				// split the keyword and data pair
-				String keyword = data.substring(0, delimiter);
-				String value = data.substring(delimiter + 1, data.length()).trim();
-				
-				if (keyword.equalsIgnoreCase("Presence"))
-				{
-					presence = value;
-				}
-				else if (keyword.equalsIgnoreCase("Nickname"))
-				{
-					nickname = value;
-				}
-				else if (keyword.equalsIgnoreCase("Status"))
-				{
-					status = value;
-				}
-			}
-			
-			if (nickname != null)
-			{
-			}
-		}
 		
-		public void eventNewMessage(String source, Date created, String payload)
-		{
-			if (source == null)
-			{
-				Log.e(LOGTAG, "message source is null!");
-			}
+   };
+   
+	public boolean sendMessage(byte[] message, String endpoint, int time) throws Exception
+	{
+		Session s = mClient.getSession();
+		SingletonEndpoint destination = new SingletonEndpoint(endpoint);
+		return s.send(destination, time, new String(message));
+	}
+   
+	public class PresenceTimerTask extends TimerTask
+	{
+
+		@Override
+		public void run() {
+			byte[] payload = new byte[HEADER_SIZE];
+			ByteBuffer bb = ByteBuffer.wrap(payload);
+			bb.position(0);
+			bb.putInt(PacketType.PRESENCE.ordinal());
+			bb.putInt(0);	// no payload
+			bb.position(0);
+			bb.get(payload);
 			
-			// create a status bar notification
-			Log.i(LOGTAG, "New message received!");
+			try {
+				if(!sendMessage(payload, PRESENCE_GROUP_ID.toString(), mPresenceTTL))
+					Log.d(LOGTAG, "Could not send presence notification!");
+				Log.d(LOGTAG, "Presence notification sent!" + PRESENCE_GROUP_ID.toString() + " TTL: " + mPresenceTTL);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				Log.d(LOGTAG, "PRESENCE send exception");
+				e.printStackTrace();
 			}
-	    };
-	    
-		public void sendMessage(byte[] message, String endpoint) throws Exception
-		{
-			Session s = mClient.getSession();
-			SingletonEndpoint destination = new SingletonEndpoint(endpoint);
-			String lifetime = PreferenceManager.getDefaultSharedPreferences(mContext).getString("messageduration", "259200");
-			if (!s.send(destination, Integer.parseInt(lifetime), new String(message)))
-			{
-				throw new Exception("could not send the message");
-			}
-			else
-				Log.i(LOGTAG, "Message send!");
+			// TODO Auto-generated method stub
+			
 		}
+	};
 }
