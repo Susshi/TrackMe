5825c1e12ff96ec990b988a80bfdb379d25f51eb
Mario <m.wozenilek@tu-bs.de>
2012-07-17 15:40:48 +0200

    Improved listening for location. Takes now into account:
    - Accuracy
    - Time since last update
    - Location provider
    and their combinations
    
    Signed-off-by: Mario <m.wozenilek@tu-bs.de>

--- src/de/androidlab/trackme/listeners/TrackMeLocationListener.java
+++ src/de/androidlab/trackme/listeners/TrackMeLocationListener.java
@@ -19,6 +19,7 @@ public class TrackMeLocationListener implements LocationListener {
 	public TrackMeLocationListener(LocationDatabase db, Context appContext) {
 		this.db = db;
 		this.appContext = appContext; 
+		currentBestLocation = null;
 		LocationManager locMan = (LocationManager) this.appContext.getSystemService(Context.LOCATION_SERVICE);
 		useGPS = locMan.isProviderEnabled(LocationManager.GPS_PROVIDER);
 	}
@@ -28,8 +29,12 @@ public class TrackMeLocationListener implements LocationListener {
 	 */
 	@Override
 	public void onLocationChanged(Location newLocation) {
-		if(useGPS && newLocation.getProvider() != null && newLocation.getProvider().equals(LocationManager.NETWORK_PROVIDER)) // USE GPS only if possible
+		//if(useGPS && newLocation.getProvider() != null && newLocation.getProvider().equals(LocationManager.NETWORK_PROVIDER)) // USE GPS only if possible
+			//return;
+		if(!isBetterLocation(newLocation, currentBestLocation))
 			return;
+		else
+			currentBestLocation = newLocation;
 		
 		Log.i("DATABASE","New Position: Lat: " + Double.toString(newLocation.getLatitude()) + "Long: " + Double.toString(newLocation.getLongitude()));
 		String phoneNumber, hash;
@@ -74,14 +79,74 @@ public class TrackMeLocationListener implements LocationListener {
 		System.out.println("Status changed: " + arg0);
 
 	}
+
+	/** Determines whether one Location reading is better than the current Location fix
+	  * @param location  The new Location that you want to evaluate
+	  * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+	  */
+	protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+	    if (currentBestLocation == null) {
+	        // A new location is always better than no location
+	        return true;
+	    }
+
+	    // Check whether the new location fix is newer or older
+	    long timeDelta = location.getTime() - currentBestLocation.getTime();
+	    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+	    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+	    boolean isNewer = timeDelta > 0;
+
+	    // If it's been more than two minutes since the current location, use the new location
+	    // because the user has likely moved
+	    if (isSignificantlyNewer) {
+	        return true;
+	    // If the new location is more than two minutes older, it must be worse
+	    } else if (isSignificantlyOlder) {
+	        return false;
+	    }
+
+	    // Check whether the new location fix is more or less accurate
+	    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+	    boolean isLessAccurate = accuracyDelta > 0;
+	    boolean isMoreAccurate = accuracyDelta < 0;
+	    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+	    // Check if the old and new location are from the same provider
+	    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+	            currentBestLocation.getProvider());
+
+	    // Determine location quality using a combination of timeliness and accuracy
+	    if (isMoreAccurate) {
+	        return true;
+	    } else if (isNewer && !isLessAccurate) {
+	        return true;
+	    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+	        return true;
+	    }
+	    return false;
+	}
+
+	/** Checks whether two providers are the same */
+	private boolean isSameProvider(String provider1, String provider2) {
+	    if (provider1 == null) {
+	      return provider2 == null;
+	    }
+	    return provider1.equals(provider2);
+	}
+	
 	
 	LocationDatabase db;
 	Context appContext;
 	
 	boolean useGPS;  
 	
+	Location currentBestLocation;
+	
 	public final static long gpsMinTime = 30 * 1000;  // in millisec
 	public final static long gpsMinDistance = 10;     // in meter
 	public final static long networkMinTime = 30 * 1000; // in millisec
 	public final static long networkMinDistance = 10;    // in meter
+	private static final int TWO_MINUTES = 1000 * 60 * 2; // max time between two locations 
+														  // before the new one is taken, even 
+														  // if it is less accurate
 }
